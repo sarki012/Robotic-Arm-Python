1cac9d42c1472575468933cf2e44395dd12c30e6
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-11-04 13:19:55 -0500

    Buttons Control Servo and Stepper Motors

---
+++ roboticArm.py
@@ -0,0 +1,62 @@
+from pylibftdi import Device
+import os
+import glob
+import time
+from bluetooth import *
+import socket
+import bluetooth
+#import threading
+#from rotateThread import rotateThread
+#from boomThread import boomThread
+import tty, sys, termios
+
+wait = 0.001
+#F4:42:8F:10:5F:5F
+connection = False
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+#uuid = "00001101-0000-1000-8000-00805F9B34FB"
+advertise_service( server_sock, "raspberrypi",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ]  
+                    )
+
+filedescriptors = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin)
+#device = Device('FT71VG2G')     #Top USB
+device = Device('FT7210GA')     #Bottom USB
+device.baudrate = 115200
+device.open()
+#device.write('?\r')             #send an identity query command.
+                            #This device needs \r to end command.
+x = 0
+while 1:
+    if(connection == False):
+        print("Waiting for connection on RFCOMM channel %d" % port)
+        client_sock, client_info = server_sock.accept()
+        connection = True
+        print("Accepted connection from ", client_info)
+    try:  
+        data = client_sock.recv(1024)
+    #    print(data)
+        device.write(data)
+     #   if data == "disconnect":
+      #      print("Client wanted to disconnect")
+       #     client_sock.close()
+        #    connection = False
+      #  if(data == 'u'):
+       #     device.write('u')
+        #elif(data == 'd'):
+         #   device.write('d')
+
+    except KeyboardInterrupt:
+        print("\nDisconnected")
+        client_sock.close()
+        server_sock.close()
+        break      
+termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
